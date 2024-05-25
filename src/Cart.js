3d5b2dbbad80b60748c96eb1736574eb998ff836
Mushad Ahmed <mushadahmed23@gmail.com>
2024-05-25 12:17:22 +0530

    Added Delete Functionality

--- src/Cart.js
+++ src/Cart.js
@@ -3,16 +3,94 @@ import CartItem from "./CartItem";
 
 class Cart  extends React.Component
 {
+constructor(){
+
+super();
+this.state={
+products:[
+    {
+        id:1,
+        price:999,
+        title:'Phone',
+        qty:1,
+    },
+    {   id:2,
+        price:99,
+        title:'Watch',
+        qty:4
+    },
+    {
+        id:3,
+        price:2999,
+        title:'Televison',
+        qty:3
+    }
+]
+}; 
+
+}
+ handleIncreaseQuantity=(product)=>{
+    
+const {products}=this.state;
+const index= products.indexOf(product);
+
+products[index].qty+=1;
+
+this.setState({
+    products:products
+})
+}
+
+handleDecreaseQuantity=(product)=>{
+    
+    const {products}=this.state;
+    const index =products.indexOf(product);
+    
+    
+    if( products[index].qty>0){
+        products[index].qty-=1;
+    }
+    
+    this.setState({
+        products:products
+    })
+}
+
+handleDeleteProduct=(id)=>{
+const {products}=this.state;
+// const index = products.indexOf(product);
+// console.log(index)
+// products.splice(index,1);
+const items = products.filter((item)=>item.id!==id)
+
+this.setState({
+    products:items
+})
+
+}
+
 
 render(){
+const { products}= this.state;
 return(
-    <div class='cart' >
-<CartItem></CartItem>
-<CartItem></CartItem>
+<div className='cart' >
+{ products.map((product)=>{
+    return(<CartItem
+        product={product}
+        key={product.id}
+        onIncreaseQuantity={this.handleIncreaseQuantity}
+        onDecreaseQuantity={this.handleDecreaseQuantity}
+        onDeleteProduct={this.handleDeleteProduct}
+
+        ></CartItem>)
+
+})}
+
+
 </div>
 )
 
 }
 }
 
-export default Cart
+export default Cart;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,80 +1,80 @@
 import React from "react";
 class CartItem extends React.Component{
 
-    constructor(){
-        super();
-        this.state={
-            price:999,
-            title:'Phone',
-            qty:0,
-            
-        }
-    }
-    increaseQuantity=()=>{
-        //1way for setting state
-        // this.setState({
-        //     qty:this.state.qty+1
-        // })
 
-        // 2nd way of setting state
-        // note:- set state is async in nature we need to use another call back func for doing sync task
-        // except promises and ajax  and render is called eve ry time
-        this.setState ((prevState)=>{
-           return{ qty:prevState.qty+1
 
-           };
-        });
-    };   // we are arrow function for binding
-       // this.increaseQuantity = this.increaseQuantity.bind(this)
+// increaseQuantity=()=>{
+//     //1way for setting state
+//     // this.setState({
+//     //     qty:this.state.qty+1
+//     // })
+
+//     // 2nd way of setting state
+//     // note:- set state is async in nature we need to use another call back func for doing sync task
+//     // except promises and ajax  and render is called eve ry time
+//     this.setState ((prevState)=>{
+//         return{ qty:prevState.qty+1
+
+//         };
+//     });
+// } // we are arrow function for binding
+//     // this.increaseQuantity = this.increaseQuantity.bind(this)
 
 
 
-       decreaseQuantity= ()=>{
-        this.setState((prevState)=>{
-            return{
-              
-                qty:prevState.qty>0 ?prevState.qty-1:prevState.qty
-            
-        }
+// decreaseQuantity= ()=>{  
+// this.setState((prevState)=>{
+//     return{
+        
+//         qty:prevState.qty>0 ?prevState.qty-1:prevState.qty
+    
+// }
 
-        });
-       }
-    render(){
-        const { price,title,qty}=this.state; //object destructuring 
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style={styles.image}/>
-                </div>
-                <div className="right-block">
-                    <div style={{fontSize:25}}>{title}</div>
-                    <div style={{color:"#777", }}>Rs {  price}</div>
-                    <div style={{color:"#777 "}}>Qty: {qty} </div>
-                    <div className="cart-item-actions">
-                         <img alt="increase"
-                          className="action-icons" 
-                           src="https://www.svgrepo.com/show/526461/add-circle.svg"
-                         // onClick={this.increaseQuantity.bind(this)}
-                         onClick={this.increaseQuantity}
-                          />
-                         <img alt="decrease" 
-                         className="action-icons"
-                          src="https://www.svgrepo.com/show/525444/minus-circle.svg"
-                          onClick={this.decreaseQuantity}
-                          />
-                         <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/9581/9581117.png"/> 
-                    </div>
-                </div>
+// });
+// }
+
+
+render(){
+const { price,title,qty}=this.props.product; //object destructuring 
+return (
+    <div className="cart-item">
+        <div className="left-block">
+            <img style={styles.image}/>
+        </div>
+        <div className="right-block">
+            <div style={{fontSize:25}}>{title}</div>
+            <div style={{color:"#777", }}>Rs {  price}</div>
+            <div style={{color:"#777 "}}>Qty: {qty} </div>
+            <div className="cart-item-actions">
+                    <img alt="increase"
+                    className="action-icons" 
+                    src="https://www.svgrepo.com/show/526461/add-circle.svg"
+                    // onClick={this.increaseQuantity.bind(this)}
+                    onClick={()=>{this.props.onIncreaseQuantity(this.props.product)}}
+                    />
+                    <img alt="decrease" 
+                    className="action-icons"
+                    src="https://www.svgrepo.com/show/525444/minus-circle.svg"
+                    onClick={()=>{this.props.onDecreaseQuantity(this.props.product)}}
+                    />
+                    <img alt="delete"
+                        className="action-icons"
+                        src="https://cdn-icons-png.flaticon.com/128/9581/9581117.png"
+                        onClick={()=>this.props.onDeleteProduct(this.props.product.id)}
+                    /> 
             </div>
-        )
-    }
+        </div>
+    </div>
+)
+}
+
 }
 const styles={
-    image:{
-        height:110,
-        width:110,
-        borderRadius:4,
-        backgroundColor:'red' 
-    }
+image:{
+height:110,
+width:110,
+borderRadius:4,
+backgroundColor:'red' 
 }
+} 
 export default CartItem;
